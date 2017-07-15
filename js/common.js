522d5722cdc0c82899ee36ede3de2c671f83d47b
Bogdan Peregon <peregonbw@gmail.com>
2017-07-16 02:28:42 +0300

    v0.3
    minor v2

--- js/common.js
+++ js/common.js
@@ -7,3 +7,12 @@ $(document).ready(function(){
 $('.hamburger').click(function(){
 $('#nav').slideToggle({top: 'toggle'});
 });
+$(window).resize(function(){
+    var winwidth = $(window).innerWidth();
+    if(winwidth > 992){
+        $('ul#nav.navbar').css( "display", "block"); 
+    }
+    else if(winwidth < 992){
+        $('ul#nav.navbar').css( "display", "none");  
+    }
+});
