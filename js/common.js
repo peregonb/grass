1bc428b0991a028dd10299ede29aa4a02a12ec1e
Bogdan Peregon <peregonbw@gmail.com>
2017-07-16 02:10:07 +0300

    v0.2
    minor update

--- js/common.js
+++ js/common.js
@@ -1,8 +1,9 @@
-var width_before = $(".search").width;
-var width_after = width_before - 50;
 $(document).ready(function(){
   $(".hamburger").click(function(){
     $(this).toggleClass("is-active");
   });
-  $(".width_search").css("width", width_after);
+});
+
+$('.hamburger').click(function(){
+$('#nav').slideToggle({top: 'toggle'});
 });
