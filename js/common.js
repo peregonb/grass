b58b3320dae3d74db1ed0ecd12f47221edef8343
Bogdan Peregon <peregonbw@gmail.com>
2017-07-17 17:54:52 +0300

    v0.5
    minooor

--- js/common.js
+++ js/common.js
@@ -16,3 +16,36 @@ $(window).resize(function(){
         $('ul#nav.navbar').css( "display", "none");  
     }
 });
+
+$('#owl-carousel-1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('#owl-carousel-2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
