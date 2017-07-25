12e00488c048c20d5224571be6740535dbacf94b
Bogdan Peregon <peregonbw@gmail.com>
2017-07-25 14:06:04 +0300

    v.0.1.1.1.1
    minor updates

--- js/common.js
+++ js/common.js
@@ -152,3 +152,29 @@ $(this).find(".novelties-plus + .novelties-plus-extended").css("display", "flex"
     $(this).find(".novelties-plus + .novelties-plus-extended").css("display", "flex");
     $(this).find(".novelties-plus + .novelties-plus-extended").animate({ width: 'toggle' }, 80);
 });
+$.fn.hyphenate = function() {
+  var all = "[абвгдеёжзийклмнопрстуфхцчшщъыьэюя]",
+    glas = "[аеёиоуыэю\я]",
+    sogl = "[бвгджзклмнпрстфхцчшщ]",
+    zn = "[йъь]",
+    shy = "\xAD",
+    re = [];
+   
+  re[1] = new RegExp("("+zn+")("+all+all+")","ig");
+  re[2] = new RegExp("("+glas+")("+glas+all+")","ig");
+  re[3] = new RegExp("("+glas+sogl+")("+sogl+glas+")","ig");
+  re[4] = new RegExp("("+sogl+glas+")("+sogl+glas+")","ig");
+  re[5] = new RegExp("("+glas+sogl+")("+sogl+sogl+glas+")","ig");
+  re[6] = new RegExp("("+glas+sogl+sogl+")("+sogl+sogl+glas+")","ig");
+  return this.each(function() {
+    var text = $(this).html();
+    for (var i = 1; i < 7; ++i) {
+      text = text.replace(re[i], "$1"+shy+"$2");
+    }
+    $(this).html(text);
+  });
+};
+
+// $(function() {
+//     $('.company-today-text').hyphenate();
+// })
